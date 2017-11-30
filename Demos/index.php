505628baddaa90534f482424e523777b97aa8223
jedepaepe <jedepaepe@PC9768.domepfc.net>
2017-11-30 21:10:33 +0100

    Setup du lien Netbeans - gir repository

---
+++ Demos/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Initialisation répository github
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Git check</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello git";
+        ?>
+    </body>
+</html>
