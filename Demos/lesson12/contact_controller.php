6e829de862af04a51be1802f829a7c8a9282e31c
Jean Michel Depaepe <jedepaepe@domepfc.net>
2017-12-07 21:52:50 +0100

    update

---
+++ Demos/lesson12/contact_controller.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        var_dump($_POST);
+        echo "<br>";
+        echo $_POST["email"];
+        ?>
+    </body>
+</html>
---
+++ Demos/lesson12/contact_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="contact_controller.php" method="POST">
+            <input type="email" name="email" placeholder="robert@gmail.com"><br>
+            <input type="text" name="prenom" placeholder="Robert"><br>
+            <input type="submit" value="envoyer">
+        </form>
+    </body>
+</html>
