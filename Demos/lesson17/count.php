c6dfe740de78de0f3f8eaaf9720467d2b52b2383
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-25 22:26:27 +0100

    Update count.php
    commentaire

--- Demos/lesson17/count.php
+++ Demos/lesson17/count.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * compte le nombre de chargement de la page (= nombre exécution du script)
+ */
 // rem: génére un code HTML non valide
 if( ! isset($_COOKIE['count'])) {   // si le COOKIE 'count' n'existe pas => c'est le premier chargement de la page
     $count = 1;                     // initialiser la variable $count à 1
