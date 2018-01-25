9ae6fa74ddc34f1ea14305d2267151678ab9f1af
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-25 22:22:22 +0100

    Update count.php
    Ajout de commentaires

--- Demos/lesson17/count.php
+++ Demos/lesson17/count.php
@@ -1,9 +1,9 @@
 <?php
 // rem: génére un code HTML non valide
-if( ! isset($_COOKIE['count'])) {
-    $count = 1;
-} else {
-    $count = $_COOKIE['count'] + 1;
+if( ! isset($_COOKIE['count'])) {   // si le COOKIE 'count' n'existe pas => c'est le premier chargement de la page
+    $count = 1;                     // initialiser la variable $count à 1
+} else {                            // si le COOKIE 'count' exist => la page est chargée une nouvelle fois
+    $count = $_COOKIE['count'] + 1; // assigner le COOKIE 'count' à la variable $count et ajouter 1
 }
-setcookie("count", $count);
-echo $count;
+setcookie("count", $count);         // écrire le cookie
+echo "La page a été chargée $count fois";                        // afficher le nombre de chargements
