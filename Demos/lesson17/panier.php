22c61d9d660620bd8f58eb5efd9992e3db44dc3d
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-25 22:25:41 +0100

    Update panier.php
    Ajout de commentaires

--- Demos/lesson17/panier.php
+++ Demos/lesson17/panier.php
@@ -1,18 +1,26 @@
 <?php
+/**
+ * Simulation d'un panier:
+ * le formulaire 'item' sert à ajouter des items (marchandises) au panier
+ * le cookie 'panier' sert à enregistrer le panier
+ */
 var_dump($_POST);   // debug
 var_dump($_COOKIE); // debug
 //
+// récupérer l'item (la marchandises)
 // $item = $_POST['item']; // crash si $_POST['item'] n'existe pas => utiliser la ligne suivante
 $item = filter_input(INPUT_POST, "item", FILTER_SANITIZE_STRING); // sécurisé
 
+// récupérer le panier
 // $panier = $_COOKIE['panier']; // crash si $_COOKIER['panier'] n'existe pas
 $panier = filter_input(INPUT_COOKIE, "panier", FILTER_SANITIZE_STRING);
 
-
+// mettre à jour le panier
 // doit être conditionnel (ajouter if ...)
 // bahamas;mont blanc;thalasso
 $panier = $panier . ";" . $item;
 
+// enregistrer le panier dans le COOKIE 'panier'
 setcookie('panier', $panier);
 
 ?>
@@ -23,8 +31,9 @@ setcookie('panier', $panier);
         <title></title>
     </head>
     <body>
+        <!-- formulaire simulant l'ajout d'un produit dans un panier-->
         <form action="panier.php" method="POST">
-            <input name="item">
+            <input name="item">  <!-- il est clair que dans un cas réel, l'utilisateur ne pourra pas écrire ce qu'il veut : il devra choisir le produit dans une liste-->
             <input type="submit" value="submit">
         </form>
     </body>
