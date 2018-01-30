1e21e2de0dcd4757afdde3caebf571e3236fed9c
MarcBouchonville <marcbouchonville@gmail.com>
2018-01-30 17:26:28 +0100

    maj 30-01-2018 panier2.php

---
+++ Demos/Lesson17/panier2.php
@@ -0,0 +1,69 @@
+<?php
+
+var_dump($_POST);
+
+/* $item = $_POST['item']; // crash si n'existe pas encore */
+$a = false;
+
+$item= filter_input(INPUT_POST, "item",FILTER_SANITIZE_STRING);
+/* $panier = $_COOKIE['panier'];  // crash si n'existe pas encore */
+
+$panier = filter_input(INPUT_COOKIE,"panier",FILTER_SANITIZE_STRING);
+
+
+// ajouter un IF pour éviter de commencer par ";" si panier n'existe pas encore
+// pour la ligne qui suit :
+
+switch ($item)
+{
+    case "bahamas":
+        $a = true ;
+        echo "item : " . $item . "\n";
+        break;
+    case "mont blanc" :
+        $a = true ;
+        break;
+    case "thalasso" :
+        $a = true ;
+        break;
+    default :
+        echo 'Choisir entre bahamas, mont blanc OU thalasso !' . "\r" . "\n";
+}
+
+if ($a) {
+    echo '$a = ' . "$a" . "\r" . "\n";
+    if ($panier=='') {
+        $panier = $item ;
+    } else {
+        $panier = $panier . " ; " . $item;
+    }
+}
+
+setcookie('panier', $panier);
+echo 'Le panier contient : ' . $panier . "\r" . "\n";
+echo '*****' . "\r" . "\n";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PANIER2 PHP</title>
+    </head>
+    <body>
+        <form action="panier2.php" method="POST">
+            <input type="text" name="item" />
+            <input type="submit" value="envoyer"/>
+        </form>
+        
+        <h3>resultat</h3>
+        
+        <?php
+            echo "panier : " . $panier;
+            echo "\n";
+            echo "item : " . $item;
+        ?>
+        
+    </body>
+</html>
