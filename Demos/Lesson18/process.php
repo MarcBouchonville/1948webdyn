d3c10ea553b01a9f0bdc15692d28062a3ad6bd0c
MarcBouchonville <marcbouchonville@gmail.com>
2018-02-03 17:53:11 +0100

    2eme maj 03-02-2018 lesson18

---
+++ Demos/Lesson18/process.php
@@ -0,0 +1,46 @@
+<?php
+$valid = isset($_POST['nom']) && isset($_POST['email']); 
+        // le formulaire est-il rempli
+
+if ($valid) {       // si le formulaire est rempli
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+        // nettoie $_POST['nom') et assigne à $nom
+    
+    setcookie('nom', $nom, time() + 3600 * 24, "/", null, false, true);
+        // crée le cookie (ou le mettre à jour)
+    
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+        // idem pour email
+    
+    setcookie('email', $email, time() + 3600 * 24, "/", null, false, true);
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Process</title>
+    </head>
+    <body>
+        <?php
+        if ($valid) {
+            ?>
+            <section id="valid">
+                <p>
+                    Félicitation, vous être enregistré sous:<br>
+                    <?php echo $nom ?><br>
+                    <?php echo $email ?><br>
+                    <a href="content.php">continuer</a>
+                </p>
+            </section>
+        <?php } else { ?>
+            <section id="invalide">
+                <p>
+                    Erreur<br>
+                    veuillez introduire votre nom et email<br>
+                    <a href='sign.php'>click ici pour signer</a>
+                </p>
+            </section>
+        <?php } ?>
+    </body>
+</html>
--- Demos/Lesson18/sign.php
+++ Demos/Lesson18/sign.php
@@ -7,16 +7,36 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>1-Sign</title>
+            <script type="text/javascript">
+                    function verif(nom, email)
+                    {
+                            if ((nom === undefined) || (email === undefined))
+                            {
+                                    alert("les 2 champs doivent être complétés !");
+                                    return false;
+                            } else {
+                                    alert("ok");
+                                    return true;
+                            }
+                    }
+            </script>
     </head>
     <body>
         <form action="process.php" method="POST">
-            <label></label>
-            <input type="text" name="item" />
-            <input type="submit" value="envoyer"/>
+            <div>
+                <label>Nom</label>
+                <input type="text" name="nom" placeholder="votre nom" />
+                <br>
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email" name="email" placeholder="adresse email" />
+                <br>
+            </div>
+            <div>
+                <input type="submit" value="Ok" onclick="verif(nom, email);"/>
+            </div>
         </form>
-        <?php
-        // put your code here
-        ?>
     </body>
 </html>
