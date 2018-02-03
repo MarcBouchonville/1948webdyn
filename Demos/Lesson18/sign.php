e145a397cbe258b5bda86977dc97210e3b60990c
MarcBouchonville <marcbouchonville@gmail.com>
2018-02-03 16:22:09 +0100

    1ere maj 03-02-2018 lesson18

---
+++ Demos/Lesson18/sign.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="process.php" method="POST">
+            <label></label>
+            <input type="text" name="item" />
+            <input type="submit" value="envoyer"/>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
