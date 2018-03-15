2eb67fb9fdf27eb2d2362d62fbb0bf79586aa449
jedepaepe <jedepaepe@PC9768.domepfc.net>
2018-03-15 20:40:05 +0100

    lesson 23

---
+++ Demos/lesson23battleship1/game.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Battle Ship</title>
+        <link href="style/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1>Battle Ship</h1>
+        <main>
+            <div id="ships" class="ships">
+                <div>
+                    <div></div><div></div><div></div><div></div>
+                </div>
+                <div>
+                    <div></div><div></div><div></div>
+                </div>
+                <div>
+                    <div></div><div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+            <div id="other" class="board">
+
+            </div>
+            <div id="me" class="board">
+
+            </div>
+        </main>
+        <script src="javascript/board.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ Demos/lesson23battleship1/javascript/board.js
@@ -0,0 +1,6 @@
+var content = "";
+for (var i = 0; i < 100; i++) {
+    content += "<div></div>";
+}
+document.getElementById("other").innerHTML = content;
+document.getElementById("me").innerHTML = content;
