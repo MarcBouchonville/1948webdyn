ea8d49274577627bbe8705c9adbbde0967bfc2a6
2502mabouchonville <2502mabouchonville@PC9781.domepfc.net>
2018-03-22 19:07:53 +0100

    Lesson24 maj nr1

---
+++ Demos/Lesson24/btClick002.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE HTML5>
+<HTML>
+    <head>
+        <title>
+            test
+        </title>
+
+
+    </head>
+    <body>
+        <div>
+            <p id="demo">couleur</p>
+        </div>
+        
+        
+        <button id="bouton1">Click me</button>
+        <script>
+            document.getElementById("bouton1").onclick = function(){
+                var recherche = document.getElementById("bouton1");
+                recherche.style.color = "green";
+                recherche.style.backgroundColor = "red";
+                
+            }
+        </script>
+    </body>
+</HTML>
