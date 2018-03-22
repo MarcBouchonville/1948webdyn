8a065c673c931aa49050cf3efd05d863af364cb9
jedepaepe <jedepaepe@PC9768.domepfc.net>
2018-03-22 19:20:08 +0100

    Preparation Lesson 24: boards with one click

--- Demos/lesson23battleship1/javascript/board.js
+++ Demos/lesson23battleship1/javascript/board.js
@@ -1,6 +1,18 @@
 var content = "";
-for (var i = 0; i < 100; i++) {
-    content += "<div></div>";
+for (var i = 0; i < 10; i++) {
+    content += "<div>";
+    for (var j = 0; j < 10; j++) {
+        content += "<div class='cell'></div>";
+    }
+    content += "</div>";
 }
 document.getElementById("other").innerHTML = content;
 document.getElementById("me").innerHTML = content;
+var cellList = document.getElementsByClassName('cell');
+for(var i=0; i < cellList.length; i++) {
+    cellList[i].addEventListener('click', cellClicked);
+}
+
+function cellClicked(ev) {
+    ev.target.style.backgroundColor = "white";
+}
