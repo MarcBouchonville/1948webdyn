977efa689562c9f10f980f1ff7ab60e0ab9f608a
jedepaepe <jedepaepe@PC9768.domepfc.net>
2018-03-29 21:28:43 +0200

    lesson 25

---
+++ Demos/lesson25/javascript/board.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function createCell(x, y, board) {
+    return "<div data-x=" + x + " data-y=" + y + " data-boat=false data-board='" + board + "'></div>";
+}
