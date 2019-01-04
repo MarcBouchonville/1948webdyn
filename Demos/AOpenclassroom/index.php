66810fc25c2717126eec1aafb13728d66746a615
MarcBouchonville <marcbouchonville@gmail.com>
2019-01-04 11:50:11 +0100

    mise à jour des problemes 04-01-2019

---
+++ Demos/AOpenclassroom/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>index</title>
+    </head>
+    <body>
+        
+        <a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a>
+        
+    </body>
+</html>
